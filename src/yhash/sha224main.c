654b026000f4d4ad611d412f64d9042583720185
yaspr <binary.yaspr@gmail.com>
2019-10-11 22:24:35 +0000

    First commit :)

---
+++ src/yhash/sha224main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "hash.h"
+
+#include "rdtsc.h"
+
+//
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+    return printf("%s [input file]\n", argv[0]), -1;
+
+  struct stat sb;
+
+  if (stat(argv[1], &sb) == -1)
+    return printf("Error: Cannot open file (%s)\n", argv[1]), -2;
+
+  u_int64 len = sb.st_size;
+  FILE *fd = fopen(argv[1], "rb");
+  byte hash[SHA224_H_SIZE] __attribute__((aligned(ALIGN)));
+
+  if (!fd)
+    ;
+
+  byte *buffer = malloc(sizeof(byte) * len);
+
+  fread(buffer, sizeof(byte), len, fd);
+  
+  double b = rdtsc();
+  
+  sha224hash(buffer, len, hash);
+
+  double a = rdtsc();
+  
+  printhash(hash, SHA224_H_SIZE);
+
+  printf("Cycles     : %lf\n", (a - b));
+  printf("Cycles/Byte: %lf\n", (a - b) / len);
+  printf("Bytes/Cycle: %lf\n", len / (a - b));
+
+  free(buffer);
+
+  fclose(fd);
+  
+  return 0;
+}
